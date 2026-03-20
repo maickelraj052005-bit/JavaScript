e07a478c61b306540fa1dbddd3d02cf079f4faa0
maickelraj S <maickelraj052005@gmail.com>
2026-03-20 21:14:43 +0530

    Add files via upload

---
+++ Day1.js/heighestnumber.js
@@ -0,0 +1,13 @@
+let a=20,b=50,c=40;
+
+if (a>b && a>c) {
+    console.log("A is high number");
+}
+else if(b>a  && b>c)
+    {
+    console.log("B is high number");
+}
+else{
+    
+    console.log("C is high number");
+}
