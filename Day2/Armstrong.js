1ec45a0810f98b3baabf8d73e785cac97b3d5604
maickelraj S <maickelraj052005@gmail.com>
2026-03-26 22:20:40 +0530

    Add files via upload

---
+++ Day2/Armstrong.js
@@ -0,0 +1,23 @@
+let original=153, lastDigit,sum=0,num;
+
+
+ num = original;
+while(num>0)
+{
+    lastDigit= num%10;
+    sum=sum+Math.pow(lastDigit,3)
+    num=Math.floor(num/10)
+}
+let lastResult;
+
+if(original===sum)
+    {
+        lastResult = original+" is a amstrong";
+    }
+else
+{
+    lastResult = original+" is not a amstrong";
+}
+console.log(lastResult);
+document.getElementById("result").innerText=lastResult;
+
