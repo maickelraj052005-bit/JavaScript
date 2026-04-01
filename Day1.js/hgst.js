c6cddc7e9b929b2fffd25612e2d33b00f2b23d17
maickelraj S <maickelraj052005@gmail.com>
2026-04-01 22:14:29 +0530

    Add files via upload

---
+++ Day1.js/hgst.js
@@ -0,0 +1,15 @@
+let num=5482
+let largest=0,smallest=9
+while (num>0)
+{
+ let digit = num%10;
+ if (digit>largest) {
+    largest=digit
+ }   
+ if (digit<smallest) {
+    smallest=digit;
+ }
+ num=Math.floor(num/10);
+}
+console.log(largest);
+console.log(smallest);
